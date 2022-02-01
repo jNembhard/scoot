2bd43eb710e9b43710605f64a266984896e9634b
jNembhard <nembhardjl@outlook.com>
2022-02-01 00:01:44 -0500

    themes

---
+++ src/styles/theme.js
@@ -0,0 +1,18 @@
+export const theme = {
+  breakpoints: {
+    tablet: `(min-width: 767px)`,
+    laptop: `(min-width: 1200px)`,
+    desktop: `(min-width: 1440px)`,
+  },
+
+  colors: {
+    yellow: "#FCB72B",
+    darkNavy: "#495567",
+    dimGrey: "#939CAA",
+    lightGrey: "#E5ECF4",
+    snow: "#F2F5F9",
+    lightYellow: "#FFF4DF",
+  },
+
+  fonts: { regular: "400", bold: "700" },
+};
